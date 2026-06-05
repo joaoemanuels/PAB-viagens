aeadf545cebd907cefa3949ffdc4981083bf0bfa
joaoemanuels <jemanuel.pi@gmail.com>
2026-06-05 10:27:48 -0300

    fix

--- src/components/ui/ModalAdmin/index.jsx
+++ src/components/ui/ModalAdmin/index.jsx
@@ -1,3 +1,6 @@
+import { NavLink, useNavigate } from "react-router-dom";
+import { authService } from "../../../services/auth";
+import { passengersData } from "../../../data/passengers";
 import {
   BadgeQuestionMark,
   CreditCard,
@@ -6,12 +9,11 @@ import {
   Ticket,
   User,
 } from "lucide-react";
-import { NavLink, useNavigate } from "react-router-dom";
-import styles from "./modalAdmin.module.css";
+
 import Icon from "../../../assets/images/favicon.svg";
-import { passengersData } from "../../../data/passengers";
 import AppVersion from "../AppVersion";
-import { authService } from "../../../services/auth";
+
+import styles from "./modalAdmin.module.css";
 
 export default function ModalAdmin({ isOpen, onClose }) {
   const navigate = useNavigate();
@@ -37,7 +39,10 @@ export default function ModalAdmin({ isOpen, onClose }) {
       <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
         <div className={styles.profileHeader}>
           <div className={styles.avatarWrapper}>
-            <img src={passengersData.avatar} alt="Ricardo de Oliveira" />
+            <img
+              src="https://scontent.cdninstagram.com/v/t51.82787-19/642493145_18055397795454507_6225200326203174088_n.jpg?_nc_cat=104&ccb=7-5&_nc_sid=bf7eb4&efg=eyJ2ZW5jb2RlX3RhZyI6InByb2ZpbGVfcGljLnd3dy4xMDgwLkMzIn0%3D&_nc_ohc=Dpodp99Q7dwQ7kNvwEEcMQh&_nc_oc=AdoQBXN96OBCVZQMNyNb5pbILUHNin48VTmlVUW-J1xxMC8wyY2buZETYmZZU4PKy14&_nc_zt=24&_nc_ht=scontent.cdninstagram.com&_nc_gid=9iKtou7INYlnPps64IULpw&_nc_ss=7c6a8&oh=00_Af8Hg_6KIz8NicoJw66T3mtmG8wP4N0t6W-em_7nZU8mzw&oe=6A2895D2"
+              alt="Paulo Aguiar"
+            />
             <span>
               <Star fill={"#fff"} size={12} />
             </span>
--- src/pages/admin/ProfileAdmin/sections/DriverProfile/index.jsx
+++ src/pages/admin/ProfileAdmin/sections/DriverProfile/index.jsx
@@ -5,14 +5,13 @@ export default function DriverProfile({
   name = "Motorista",
   rating = 0,
   tripsCount = "0",
-  avatarUrl = "",
 }) {
   return (
     <section className={styles.profileAdmin}>
       <div className={styles.profileCard}>
         <div className={styles.avatarWrapper}>
           <img
-            src={avatarUrl}
+            src="https://scontent.cdninstagram.com/v/t51.82787-19/642493145_18055397795454507_6225200326203174088_n.jpg?_nc_cat=104&ccb=7-5&_nc_sid=bf7eb4&efg=eyJ2ZW5jb2RlX3RhZyI6InByb2ZpbGVfcGljLnd3dy4xMDgwLkMzIn0%3D&_nc_ohc=Dpodp99Q7dwQ7kNvwEEcMQh&_nc_oc=AdoQBXN96OBCVZQMNyNb5pbILUHNin48VTmlVUW-J1xxMC8wyY2buZETYmZZU4PKy14&_nc_zt=24&_nc_ht=scontent.cdninstagram.com&_nc_gid=9iKtou7INYlnPps64IULpw&_nc_ss=7c6a8&oh=00_Af8Hg_6KIz8NicoJw66T3mtmG8wP4N0t6W-em_7nZU8mzw&oe=6A2895D2"
             alt={`Foto de ${name}`}
             className={styles.avatarImage}
           />
@@ -23,7 +22,7 @@ export default function DriverProfile({
           <h2 className={styles.driverName}>{name}</h2>
 
           <div className={styles.metaRow}>
-            <div className={styles.ratingBadge}>
+            <div className={styles.ratingBadge || "5.0"}>
               <Star size={12} fill="#059669" stroke="#059669" />
               <span>{rating.toFixed(1)}</span>
             </div>
